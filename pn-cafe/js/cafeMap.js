c6c136900b998acdac8854eb884ee29ffdccb9de
yasufumi-iki <iki.suruga@gmail.com>
2016-04-22 17:00:30 +0900

    Added files via upload

---
+++ pn-cafe/js/cafeMap.js
@@ -0,0 +1,30 @@
+var map;
+$(document).ready(function(){
+  map = new GMaps({
+    div: '#googlemap',
+    lat: -12.043333,
+    lng: -77.028333
+  });
+  map.addMarker({
+    lat: -12.043333,
+    lng: -77.03,
+    title: 'Lima',
+    details: {
+      database_id: 42,
+      author: 'HPNeo'
+    },
+    click: function(e){
+      if(console.log)
+        console.log(e);
+      alert('You clicked in this marker');
+    }
+  });
+  map.addMarker({
+    lat: -12.042,
+    lng: -77.028333,
+    title: 'Marker with InfoWindow',
+    infoWindow: {
+      content: '<p>HTML Content</p>'
+    }
+  });
+});
